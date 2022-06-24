3717ed442721389aca537f8fcccb6d4354cd16c7
PiGuTan <pigutheboss@gmail.com>
2022-06-24 23:09:56 +0800

    added rejection logic

--- simulator.py
+++ simulator.py
@@ -1,8 +1,6 @@
 # %%
-from ast import Assert
 from logging import raiseExceptions
-import string
-from numpy import string_
+import re
 import pandas as pd
 import os
 import json
@@ -58,60 +56,108 @@ class var:
 
 del f, input
 
-class cube: #probably making a child from this
-    def read_csv():
-        for i in (var.main_line,var.sub_line):
-            try:
-                var.Lines[i]=pd.read_csv("Resources/{0}/{1}{2}.csv".format(var.equip,"Add_" if var.additional else "" ,i))
-            except FileNotFoundError:
-                raiseExceptions("No records for",i, var.equip,)
-    
-    def show_lines():
-        for x in var.Lines.items():
-            print(x[0]) #rank
-            print(list(x[1].Line)) #lines
-            print()
+class cube_lines_weight: #probably making a child from this
+    def __init__(self,line):
+        try:
+            df=pd.read_csv("Resources/{0}/{1}{2}.csv".format(var.equip,"Add_" if var.additional else "" ,line))
+        except FileNotFoundError:
+            raiseExceptions("No records for",var.main_line, var.equip,)
+
+        self.possible_lines=list(df.Line)
+        self.weight=tuple(df.Weight)
+
+    def show_lines(self):
+#!!!! change this and put this into variables
+        print(self.possible_lines)
+
+main_lines=cube_lines_weight(var.main_line)
+sub_lines=cube_lines_weight(var.sub_line)
 
 
-
-#probably puting this part into cube class
-def roll_lines(prime_rate):
-    equip_line=[]
-    for i in prime_rate:
-        if random.randint(1, 1000000)<=int(i*10000):
-            equip_line+=random.choices(list(var.Lines[var.main_line].Line), weights=tuple(var.Lines[var.main_line].Weight), k=1)# generate main line
-        else:
-            equip_line+=random.choices(list(var.Lines[var.sub_line].Line), weights=tuple(var.Lines[var.sub_line].Weight), k=1)# generate secondary line
-    return equip_line
 
 #to reject line i probably need a method to count lines
 
-cube.read_csv()
-cube.show_lines()
+main_lines.show_lines()
+sub_lines.show_lines()
 """hard coding reject class for now"""
 class reject:
     all=[]
     count=0
+    def check(self,list): 
+        match_lines=0
+        for x in list:
+            if re.match(self.string_to_reject,x):
+                match_lines+=1
+        return match_lines
     def __init__(self,string_to_reject, lines,identifier=None):
         self.string_to_reject=string_to_reject
         self.lines=lines
         self.identifier=identifier
-        all.append(self)
-        count+=1
-    def check(list):
-        pass #return boolean true if pass and false if fail 
+        if (self.check(main_lines.possible_lines)>0 or self.check(sub_lines.possible_lines)>0): #issue here always true
+            reject.all.append(self)
+            reject.count+=1
 
-test_lines=["STR", "STR","STR"]
-Decent=reject("Decent",1,"Decent skills")
-Decent.check(test_lines)
+    def check_all(cube_results):
+        for x in reject.all:
+            if x.check(cube_results)>x.lines:
+                return False
+        return True
+
+#probably puting this part into cube class
+def roll_once(prime_rate):
+    equip_line=[]
+    for i in prime_rate:
+        if random.randint(1, 1000000)<=int(i*10000):
+            #!! var.Lines[var.main_line].Line this will not work
+            equip_line+=random.choices(main_lines.possible_lines, weights=main_lines.weight, k=1)# generate main line
+        else:
+            equip_line+=random.choices(sub_lines.possible_lines, weights=sub_lines.weight, k=1)# generate secondary line
+    return equip_line
+
+def roll_lines(prime_rates):
+    roll=roll_once(prime_rates)
+    while True:
+        if not reject.check_all(roll):
+            roll=roll_once(prime_rates)
+        else:
+            return roll
+
+
+
+"""
+    def check_all(list):
+        current=True
+        for x in reject.all:
+            current=current & x.check(list)
+        return current
+"""
+
+test_lines=["Decent sharp eyes enabled", "Decent speed infusion enabled","STR","Damage when attacking boss monsters +20%", "Damage when attacking boss monsters +20%"]
+Decent=reject("Decent .* enabled",1,"Decent skills")
+Invincibility_Time=reject("Invincibility time \+[0-9] second when hit", 1, "Invincibility time")
+IED=reject("Ignore Enemy Defense \+[0-9]{1,2}%",2,"Ignore Enemy Defense %")
+Ignore_Damage_On_Hit=reject("[0-9]{1,2}% chance to ignore 40% damage when hit",2,"Damage Reduction")
+Invincibility_Chance=reject("[0-9]% chance of being invincible for 7 seconds when hit",2,"Invincibility cahnce on hit")
+Item_Drop_Rate=reject("Item Drop Rate \+[0-9]{1,2}%", 2,"Item Drop")
+Boss_Damage=reject("Damage when attacking boss monsters \+[0-9]{1,2}%", 2,"BD")
+print(Decent.check(test_lines)) #need debug this
+print(IED.check(test_lines))
+print(Ignore_Damage_On_Hit.check(test_lines))
+print(Invincibility_Chance.check(test_lines))
+print(Item_Drop_Rate.check(test_lines))
+print(Boss_Damage.check(test_lines))
+print("Last print")
+print(len(reject.all))
+print(reject.count)
+for x in reject.all:
+    print(x.string_to_reject)
+print()
+#print(reject.check_all(test_lines))
 
 """Test cubes"""
 df=[] #df represent the full data
 for x in range(var.sample_size): 
     result=roll_lines(var.prime_rates) #result is a list
-    """
-    implement rejects here
-    """
     i=0
     for y in result: #y is a string
             if y.startswith(var.desired_lines):
